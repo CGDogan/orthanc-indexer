49be8519af1555ef0c9e0a6005ba10c6d3ae2b3b
CGDogan <126820728+CGDogan@users.noreply.github.com>
2023-08-30 13:33:32 +0100

    Update Plugin.cpp

--- Sources/Plugin.cpp
+++ Sources/Plugin.cpp
@@ -217,9 +217,9 @@ static void ProcessFile(const std::string& path,
         }
 
         Json::Value upload;
-        std::string dicom;
+        /*std::string dicom;
         Orthanc::SystemToolbox::ReadFile(dicom, path);
-        OrthancPlugins::RestApiPost(upload, "/instances", dicom, false);
+        OrthancPlugins::RestApiPost(upload, "/instances", dicom, false);*/
 
         /*
         OrthancPlugins::RestApiPost(upload, "/instances", (const void *)file_memory, (size_t)sb.st_size, false);*/
